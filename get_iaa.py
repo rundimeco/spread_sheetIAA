9c1efdb9ee4453f561f5e8abcbd8754510c5fded
rundimeco <rundimeco@gmail.com>
2023-07-28 12:25:17 +0200

    Comparing lines and improving alignment

--- get_iaa.py
+++ get_iaa.py
@@ -1,53 +1,24 @@
 import glob
-import json
-import pandas
-from tools_iaa import *
 import sys
-from pathlib import Path
+from tools_iaa import *
 
-def get_df(input_files):
-  print("Files provided with appropriate extension :")
-  print(json.dumps(list(set(input_files)), indent = 2))
-  files_csv = set([x for x in input_files if Path(x).suffix in [".csv", "txt"]])
-  files_excel = set([x for x in input_files if Path(x).suffix in [".xls", ".xlsx", "ods"]])
-  print("Transforming into Pandas Dataframes ...")
-  try:
-    data_frames = [pandas.read_csv(path) for path in files_csv]
-  except:
-    print("Debugging Pandas error")
-    for path in files_csv :
-        print(path)
-        data_frames = [pandas.read_csv(path, delimiter = ";") for path in files_csv]
-  data_frames += [pandas.read_excel(path) for path in files_excel ]
-  return data_frames
-
-def get_iaa_from_files(input_files):
-  data_frames = get_df(input_files)
-  
-  annotateurs = []
-  sheet_headers = []
-  
-  for cpt_df, df in enumerate(data_frames):
-      annotations = []
-      cats = list(df.columns)
-      sheet_headers.append(cats)
-      for cpt_line, line in df.iterrows():
-          this_annot = "-"
-          for cpt_val, val in enumerate(line[1:]):
-              if pandas.isna(val)==False:
-                  this_annot = str(val)
-          annotations.append(this_annot)
-          #check double annotation
-          #check all_columns ou fusion --> si colonnes exclusives
-          #if cpt_line>100:break
-      annotateurs.append(annotations)
-
-  check_regularity(input_files, annotateurs, sheet_headers)
-
-  results = get_iaa(annotateurs)
 
 
 if __name__=="__main__":
+  from optparse import OptionParser
+  parser = OptionParser()
+  parser.add_option("-f", "--force_align",
+    action="store_true", dest="force", default=False,
+    help="Force alignment of files with different number of lines (Default: False)")
+  parser.add_option("-l", "--limit", dest="limit", default=0,
+                  help="Limit to the first X lines (Default no limit)", metavar="LIMIT")
+  (options, args) = parser.parse_args()
+  if options.force:
+      sys.argv = [x for x in sys.argv if "--force" not in x]
+  if options.limit:
+      sys.argv.remove("--limit")
+      sys.argv.remove(options.limit)
+  #print(options)
   if len(sys.argv)==1:
     dir_path = "dummy_data/csv/"
     print(f"No directory provided, using default directory : {dir_path}")
@@ -63,5 +34,5 @@ if __name__=="__main__":
   #TODO: filter out unsupported formats ?
 
   assert len(input_files)>1, NB_files_error(input_files)
-  results = get_iaa_from_files(input_files)
+  results = get_iaa_from_files(input_files, options)
 
--- tools_iaa.py
+++ tools_iaa.py
@@ -1,5 +1,67 @@
 import json
 from nltk import agreement
+import pandas
+from pathlib import Path
+
+def get_df(input_files):
+  print("Files provided with appropriate extension :")
+  print(json.dumps(list(set(input_files)), indent = 2))
+  files_csv = set([x for x in input_files if Path(x).suffix in [".csv", "txt"]])
+  files_excel = set([x for x in input_files if Path(x).suffix in [".xls", ".xlsx", "ods"]])
+  print("Transforming into Pandas Dataframes ...")
+  data_frames = []
+  for path in files_csv:
+    try:
+      data_frames.append(pandas.read_csv(path))
+    except:
+      data_frames.append(pandas.read_csv(path, delimiter = ";"))
+  data_frames += [pandas.read_excel(path) for path in files_excel ]
+  return data_frames
+
+def get_iaa_from_files(input_files, options):
+  data_frames = get_df(input_files)
+  
+  annotators = []
+  sheet_headers = []
+  tokens = [] 
+  limit_line = int(options.limit)
+  for cpt_df, df in enumerate(data_frames):
+      this_tokens = []
+      annotations = []
+      cats = list(df.columns)
+      sheet_headers.append(cats)
+      for cpt_line, line in df.iterrows():
+          this_annot = "-"
+          #discarding empty lines:
+          if pandas.isna(line[0]) or line[0]==","or line[0]=="\t":
+              limit_line+=1#To be sure to reach the limit
+              continue
+          this_tokens.append(line[0])
+          for cpt_val, val in enumerate(line[1:]):
+              if pandas.isna(val)==False:
+                  this_annot = str(val)
+          annotations.append(this_annot)
+          #TODO:check double annotation
+          #check all_columns ou fusion --> si colonnes exclusives
+          if int(options.limit)>0:
+              if cpt_line==limit_line:
+                  break
+      annotators.append(annotations)
+      tokens.append(this_tokens)
+  print("Checking alignments")
+  print([len(x) for x in tokens])
+  print([len(x) for x in annotators])
+  if int(options.limit)>0:#Making sure all annotations have the same length
+      tokens=[x[:int(options.limit)] for x in tokens]
+      annotators=[x[:int(options.limit)] for x in annotators]
+  print([len(x) for x in tokens])
+  print([len(x) for x in annotators])
+  check_tokens(tokens)
+  check_regularity(input_files, annotators, sheet_headers, options)
+  if options.force==True:#align with the smallest number of lines
+    min_annot = min([len(x) for x in annotators])
+    annotators = [x[:min_annot] for x in annotators]
+  results = get_iaa(annotators)
 
 def get_iaa(annotators, verbose = True):
   donnees = []
@@ -15,13 +77,32 @@ def get_iaa(annotators, verbose = True):
   "scotts " : ratingtask.pi()}
   if verbose :
     print(json.dumps(results, indent = 2))
+    for annot in annotators:
+        print(annot[25:35])
   return results
 
-def  check_regularity(input_files, annotators, sheet_headers):
+def check_tokens(tokens):
+  for cpt_line in range(0, len(tokens[0]), 20):
+      for cpt_file in range(len(tokens)-1):
+          list1 = [str(x) for x in tokens[cpt_file][cpt_line:cpt_line+20]]
+          list2 = [str(x) for x in tokens[cpt_file+1][cpt_line:cpt_line+20]]
+          comp_tokens(list1, list2, cpt_file, cpt_line)
+
+def comp_tokens(list1, list2, cpt_file, cpt_line):
+    if list1!=list2:
+      print(f"File {cpt_file} VS {cpt_file+1} alignement error")
+    else:
+      return True
+    for paire in zip(list1, list2):
+        print(paire, paire[0]==paire[1], cpt_line)
+        assert paire[0]==paire[1]
+        cpt_line+=1
+
+def  check_regularity(input_files, annotators, sheet_headers, options):
   #Check number of lines
   NB_annots = [len(x) for x in annotators]
-  assert min(NB_annots) == max(NB_annots), size_error(input_files, NB_annots) 
-   
+  if options.force==False:
+    assert min(NB_annots) == max(NB_annots), size_error(input_files, NB_annots) 
   print("Headers found :")
   print(sheet_headers)
   #Check headers regularity
